cba1e2a6a44b3c2e539d963286c6fffd7e3a96d8
Vyronevra-dev <pabloevra27@gmail.com>
2026-08-17 17:58:47 +0300

    Feat: added behavior to my portfolio

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const burger = document.getElementById('burger');
 const navLinks = document.getElementById('navLinks');
+const form = document.getElementById("contactForm");
+const formMsg = document.getElementById("formMsg");
+
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('open');
     navLinks.classList.toggle('open');
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const response = await fetch("https://formspree.io/f/xdenwyoq", {
+    method: "POST",
+    headers: { "Accept": "application/json" },
+    body: new FormData(form)
+  });
+
+  const result = await response.json();
+
+  if (result.ok) {
+    formMsg.style.color = "#fff4cc";
+    formMsg.textContent = "* Message sent, I'll get back to you soon!";
+    form.reset();
+  } else {
+    formMsg.style.color = "red";
+    formMsg.textContent = "* Something went wrong, try again.";
+  }
 });
