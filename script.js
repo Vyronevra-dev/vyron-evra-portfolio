f18f06b9b7fbe08d99f9e302129cff205dc46288
Vyronevra-dev <pabloevra27@gmail.com>
2026-09-04 08:41:29 +0300

    feat: added a CV to my portfolio and changed the UI of my portfolio

--- script.js
+++ script.js
@@ -211,18 +211,3 @@ async function fetchContributions() {
 }
 
 fetchContributions();
-
-// Stars
-for (let i = 0; i < 30; i++) {
-  const star = document.createElement('div');
-  star.classList.add('star');
-
-  const size = Math.random() * 3 + 1;
-  star.style.width = `${size}px`;
-  star.style.height = `${size}px`;
-  star.style.left = `${Math.random() * 100}%`;
-  star.style.top = `${Math.random() * 100}%`;
-  star.style.animationDelay = `${Math.random() * 5}s`;
-
-  starsContainer.appendChild(star);
-}
